fe7f32919e37940bef039a641c59049b04b9e5c9
Volodymyr <96985829+NQDark@users.noreply.github.com>
2022-07-22 22:41:02 +0300

    22.07_22:40

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // знаходимо елемент і записуємо його в змінну, яку можна далі використовувати
 const popup = document.querySelector('.backdrop');
 const openPopupButton = document.querySelector('.offer__button');
-const closePopupButton = document.querySelector('.point-x');
+const closePopupButton = document.querySelector('.modal__icon');
 
 // очікуємо коли якась дія виконається, в данному випадку click
 openPopupButton?.addEventListener('click', () => {
@@ -21,4 +21,29 @@ closePopupButton?.addEventListener('click', () => {
     popup.classList.remove('animated');
     // можна очікувати коли закінчиться перехід, і щось зробити
     popup.addEventListener('transitionend', () => removeAnimation(popup), { once: true });
+});
+
+// знаходимо елемент і записуємо його в змінну, яку можна далі використовувати
+const IIPopup = document.querySelector('.backdrop');
+const openIIPopupButton = document.querySelector('.offer__button');
+const closeHeaderPopupButton = document.querySelector('.modal__icon');
+
+// очікуємо коли якась дія виконається, в данному випадку click
+openIIPopupButton.addEventListener('click', () => {
+    // popup.style.visibility = 'visible'; можна на елементах безпосередньо змінювати стилі, але це погана практика
+    IIPopup.classList.add('visible'); // краще стилізувати через класи, а в js додавати або забирати такі класи
+    requestAnimationFrame(() => {  // браузер, приміняє деякі дії одночасно, тому для анімування потрібно, щоб додавання наступного класу вібдулось пізніше
+        IIPopup.classList.add('animated');
+    });
+});
+
+// function removeAnimation() { // куски кода можна групувати в функції щоб потім перевикористовувати
+//     headerPopup.classList.remove('visible');
+// }
+
+closeIIPopupButton.addEventListener('click', () => {
+    // popup.style.visibility = 'hidden';
+    IIPopup.classList.remove('animated');
+    // можна очікувати коли закінчиться перехід, і щось зробити
+    IIPopup.addEventListener('transitionend', () => removeAnimation(headerPopup), { once: true });
 });
